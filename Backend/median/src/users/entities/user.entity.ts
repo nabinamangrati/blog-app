61747b54ba5cd337a1453bd4a0292b58284d575d
nabinamangrati <mangratinabina1@gmail.com>
2024-12-02 14:42:29 +0545

    used ClassSerializerInterceptor for ommiting password from the response , also use exclude in password and update controller for returning user entity instead of prisma.user

--- Backend/median/src/users/entities/user.entity.ts
+++ Backend/median/src/users/entities/user.entity.ts
@@ -1,7 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+
 
 export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
   @ApiProperty()
   id: number;
 
@@ -16,6 +22,7 @@ export class UserEntity implements User {
 
   @ApiProperty()
   email: string;
-
+   
+  @Exclude()
   password: string;
 }
