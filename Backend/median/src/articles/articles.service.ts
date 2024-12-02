812cc014d490c85ff7f8771e63f45d44a2458b81
nabinamangrati <mangratinabina1@gmail.com>
2024-12-02 14:50:05 +0545

    returning aurthor along with article

--- Backend/median/src/articles/articles.service.ts
+++ Backend/median/src/articles/articles.service.ts
@@ -20,8 +20,12 @@ export class ArticlesService {
   }
 
   findOne(id: number) {
-    return this.prisma.article.findUnique({ where: { id } });
-  }
+    return this.prisma.article.findUnique({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });  }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
     return `This action updates a #${id} article`;
