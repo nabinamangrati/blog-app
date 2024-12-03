7b508e960e4edf139f30a39aeb73c7e611b19e4d
nabinamangrati <mangratinabina1@gmail.com>
2024-12-03 17:40:30 +0545

    salting

--- Backend/median/src/auth/auth.service.ts
+++ Backend/median/src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import {
   import { PrismaService } from './../prisma/prisma.service';
   import { JwtService } from '@nestjs/jwt';
   import { AuthEntity } from './entity/auth.entity';
+  import * as bcrypt from 'bcrypt';
   
   @Injectable()
   export class AuthService {
@@ -21,7 +22,7 @@ import {
       }
   
       // Step 2: Check if the password is correct
-      const isPasswordValid = user.password === password;
+      const isPasswordValid = await bcrypt.compare(password, user.password);
   
       // If password does not match, throw an error
       if (!isPasswordValid) {
