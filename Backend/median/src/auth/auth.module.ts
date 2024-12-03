ee6ee2efa0ac387aa7adf6185aa417d408f93d25
nabinamangrati <mangratinabina1@gmail.com>
2024-12-03 16:29:32 +0545

    Implement JWT authentication strategy

--- Backend/median/src/auth/auth.module.ts
+++ Backend/median/src/auth/auth.module.ts
@@ -4,6 +4,9 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './jwt.strategy';
+
 
 export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
 
@@ -16,8 +19,9 @@ export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
       secret: jwtSecret,
       signOptions: { expiresIn: '5m' }, // e.g. 30s, 7d, 24h
     }),
+    UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- Backend/median/src/users/users.module.ts
+++ Backend/median/src/users/users.module.ts
@@ -8,6 +8,8 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   controllers: [UsersController],
   providers: [UsersService],
   imports: [PrismaModule],
+  exports: [UsersService],
+
 
 })
 export class UsersModule {}
